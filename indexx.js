ef2272cf063d1d17fd44d38457f6b71656210b7e
Stanislav2003 <pluvec03@gmail.com>
2021-11-24 12:36:18 +0100

    ex10
    ex 10 funcions

---
+++ indexx.js
@@ -0,0 +1,84 @@
+
+/*ex1
+function fnomCognoms(nom='Stan',Cognom=' Sevdalinov'){
+console.log('Hola '+ nom +Cognom)}
+fnomCognoms();*/
+
+/*function fnom(num=10){
+console.log(typeof num)}
+fnom();*/
+
+/*function fdata(data= new Date()){
+    console.log(data)}
+    //fdata()*/
+
+/*function ffuncio(){
+    var x = 'bondia'
+    console.log(x)
+}
+console.log(x);*/
+
+/*function fvocals(paraula='hola'){
+    var nombreVocals = paraula.match(/[aeiou]/gi).length
+    console.log(nombreVocals);
+}
+fvocals();*/
+
+
+/*function parametres(a,b,c=5){
+    return a+b+c
+
+} console.log(parametres(1,2));*/
+
+/*function ex7(){
+    //cridar funcio ex3>
+    fdata()
+
+    //retornar resultat ex6
+    return parametres(1,2,3)
+}
+    console.log(ex7())*/
+
+/*function ex8(X,Y){
+
+    if(X==30 || Y==30 || X+Y==30){
+        return(true)
+    }
+}
+console.log(ex8(30,20))*/
+
+
+/*let x,y;
+x=30
+y=20
+
+
+let resultat = function(x,y){
+if(x==30 || y==30 || x+y==30){
+    return true
+}}
+
+console.log(resultat(x,y));*/
+ex 10
+/*let variable = (x,y) =>{
+    if(x===y)  return true
+    else return false}
+    console.log(variable(5,6));*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
