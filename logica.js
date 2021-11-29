b9b218e65c36b8223f57c95effc6329cb7f29f8a
Stanislav2003 <pluvec03@gmail.com>
2021-11-29 09:03:34 +0100

    ex 2

--- logica.js
+++ logica.js
@@ -1,6 +1,6 @@
 
 
-var numero = ('Introdueix un numero: ')
+/*var numero = ('Introdueix un numero: ')
 prompt (numero)
 if (( numero % 3 ) == 0){
     alert(numero + ' es multiple de 3');
@@ -9,4 +9,19 @@ if (( numero % 3 ) == 0){
 }
 else{
     alert('El numero no es multiple de 3 ni de 7');
-}
+}*/
+
+var x = 70
+var y = 25
+var preu
+
+x=(x>=10 && x<=50) ? console.log("EL numero esta en el rang")
+                   : console.log("el numero no esta en el rang")
+
+
+
+y=(y>=10 && y<=50) ? console.log("esta en el rang")
+                   : console.log("no esta en el rang")
+
+
+
