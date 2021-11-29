5a6846eabd8b4fbe237c731b02774d9bc30b2648
Stanislav2003 <pluvec03@gmail.com>
2021-11-29 09:32:51 +0100

    ex 3

--- logica.js
+++ logica.js
@@ -11,8 +11,9 @@ else{
     alert('El numero no es multiple de 3 ni de 7');
 }*/
 
-var x = 70
-var y = 25
+/*ex2 
+var x = 25
+var y =60
 var preu
 
 x=(x>=10 && x<=50) ? console.log("EL numero esta en el rang")
@@ -21,7 +22,31 @@ x=(x>=10 && x<=50) ? console.log("EL numero esta en el rang")
 
 
 y=(y>=10 && y<=50) ? console.log("esta en el rang")
-                   : console.log("no esta en el rang")
+                   :console.log("no esta en el rang ");*/
 
+
+                   var a = 7
+
+                   switch(true){
+                       case (a<5):
+                           console.log("Molt bé senyor, has suspès")
+                       break
+                   
+                       case (a>=5 && a<6):
+                           console.log("Molt bé senyor, un suficient")
+                       break
+                   
+                       case (a>=6 && a<7):
+                           console.log("Molt bé senyor, un bé")
+                           break
+                   
+                       case (a>=7 && a<9):
+                           console.log("Molt bé senyor, un notable")
+                       break
+                   
+                       case (a>=9 && a<10):
+                           console.log("Molt bé senyor, un excel·lent")
+                       break
+                   }
 
 
