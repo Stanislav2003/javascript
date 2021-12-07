6d8b2b3d04431c39dc5b9b39fe67389226614209
Stanislav2003 <pluvec03@gmail.com>
2021-12-07 12:50:08 +0100

    ex1 i ex2

---
+++ DOMJAVASCRIPT/DOMjavascript.js
@@ -0,0 +1,14 @@
+/*ex1
+var nodeTitol=document.getElementById("titol")
+
+nodeTitol.innerHTML="PRIMER TITOL"*/
+
+
+/*
+ex2
+document.write("s'esta executant a la URL:"+ "\n" + document.URL)*/
+
+
+
+
+
