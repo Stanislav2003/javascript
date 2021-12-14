393c2dfaa7c2c0a392553d194764995b14f83163
Stanislav2003 <pluvec03@gmail.com>
2021-12-14 08:54:23 +0100

    Esdeveniments ex 1,2

--- esdeveniments/esdeveniments.js
+++ esdeveniments/esdeveniments.js
@@ -1,1 +1,15 @@
-javascript
+/*function sfondo(color){
+    document.bgColor=color;
+}*/ 
+
+
+ /*document.onkeydown=function(e){
+     document.getElementById("lletra").innerHTML=e.key;
+
+ }
+ document.onkeyup=function(e){
+    document.getElementById("lletra").innerHTML=" "
+
+}*/
+
+
