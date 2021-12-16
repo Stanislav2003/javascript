adb392cb1611d22bdad898f36bd730a71aef8346
Stanislav2003 <pluvec03@gmail.com>
2021-12-16 01:01:17 +0100

    exercicis 3,4,5 esdeveniments

--- esdeveniments/esdeveniments.js
+++ esdeveniments/esdeveniments.js
@@ -4,20 +4,50 @@
 
 
  /*document.onkeydown=function(e){
-     document.getElementById("lletra").innerHTML=e.key;
+
+    document.getElementById("lletra").innerHTML=e.key;
 
  }
  document.onkeyup=function(e){
-    document.getElementById("lletra").innerHTML=" "
+    document.getElementById("lletra").innerHTML="";
 
 }*/
 
+/* ex3 
+let comb = "";
+ document.onkeydown=function(e){
+    comb=comb+e.key;
+    document.getElementById("lletra").innerHTML=comb;
 
+ }
+ document.onkeyup=function(e){
+    document.getElementById("lletra").innerHTML=comb;
+
+}
+*/
+
+
+/*ex4
 window.onload=function(){
     var contador=0;
     document.getElementById("boton").onclick=function(){
         contador++;
         document.getElementById("mostra").innerHTML=contador;
     }
-}
+}*/
+
+/*ex5
+let combinacio = "";
+
+document.onkeydown=function(e){
+    combinacio=combinacio + e.key;
+
+    if(combinacio="qwerty")
+        window.open(url="https://www.youtube.com/watch?v=RCA3EfKUJMw")
+       
+
+}*/
+
+
+
 
